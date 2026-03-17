a85d0cfbfc7dc168d2c6dcec6d6a9c1f8d0cd3d5
Nishant Pandharinath Nikam <nishantnikam.p@gmail.com>
2026-03-17 21:56:46 +0530

    Optimized code GymEx
    Today you decided to go to the gym. You currently have E energy.
    There are N exercises in the gym. Each of these exercises drains Ai
    amount of energy from your body.
    You feel tired if your energy reaches 0 or below. Calculate the
    minimum number of exercises you have to perform such that you
    become tired. Every unique exercise can only be performed at
    most 2 times as others also have to use the machines.
    If performing all the exercises does not make you feel tired, return
    
    
    Parameters:
    E INTEGER
    The first line contains an integer, E, denoting the Energy.
    E 1 I0^5
    N INTEGER
    The next line contains an integer, N, denoting the number of
    exercises.
    N 1 I0^5
    A INTEGER ARRAY
    Each line i of the N subsequent lines (where 0 <= i < N) contains an
    integer describing the amount of energy drained by ith exercise.
    Ali] 1 I0^5
    
    case 1
    Input:
    6
    2
    1
    2
    Output:
    4
    Do 1st exercise 2 times
    Do 2nd exercise 2 times
    Hence, total exercise done 4.

--- Gym.java
+++ Gym.java
@@ -1,37 +1,39 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Gym {
-    public static void main(String[] args) {
+public class Gym{
+    public static int GymEx(int E, int N, int[] arr){
+        
+        Arrays.sort(arr);
+        
+        int count = 0;
+        for(int i = N-1; i >=0; i--) {
+            if (E == 0)
+                break;
+            if (2 * arr[i] <= E) {
+                count += 2;
+                E -= 2 * arr[i];
+            }
+            else if (arr[i] <= E) {
+                    count++;
+                    E -= arr[i];
+            }
+        }
+        if(E == 0)
+            return count;
+        return -1;
+    }
 
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int E = sc.nextInt();
         int N = sc.nextInt();
-        int arr[] = new int[N];
-        int count = 0;
-        for (int i = 0; i < N; i++) {
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        for (int i = N - 1; i >= 0; i--) {
-            if (E == 0) {
-                System.out.println(count);
-            }
-            if (arr[i] * 2 <= E) {
-                count += 2;
-                E -= count;
-            } else if (arr[i] <= E) {
-                count += 1;
-                E -= count;
-            }
-        }
-        if (E == 0) {
-            System.out.println(count);
-        }
-        else
-        {
-            count=-1;
-        System.out.println(count);
-        }
+        System.out.println(GymEx(E, N, arr));
     }
+
 }
